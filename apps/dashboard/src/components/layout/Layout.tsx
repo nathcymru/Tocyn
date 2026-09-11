98c3434f6ff9b810dd18ab16c7dd48175f194674
Nathan (Cwtch Consulting) <nathcymru@gmail.com>
2026-09-11 23:17:15 +0100

    feat(dashboard): add operator workspace preferences

--- apps/dashboard/src/components/layout/Layout.tsx
+++ apps/dashboard/src/components/layout/Layout.tsx
@@ -25,7 +25,7 @@ import {
 import { useAuthStore } from '../../store/authStore';
 import { useCollaboration } from '../CollaborationContext';
 import { clsx } from 'clsx';
-import { OperatorThemeControl, OperatorThemeProvider } from '../theme/OperatorThemeProvider';
+import { OperatorPreferencesControl, OperatorThemeControl, OperatorThemeProvider } from '../theme/OperatorThemeProvider';
 
 function cn(...inputs: any[]) {
   return clsx(inputs);
@@ -98,6 +98,7 @@ function UserMenu({ onNavigate, navigationFocus }: SidebarProps) {
             Sign out of all sessions
           </TocynButton>
           <OperatorThemeControl />
+          <OperatorPreferencesControl />
         </Popover.Content>
       </Popover.Positioner>
     </div>
